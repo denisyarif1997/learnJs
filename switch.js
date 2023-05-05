b8860cb117e999d701174dd0751e58283a26ab5e
denisyarif1997 <denisyarif196@gmail.com>
2023-05-05 19:26:41 +0700

    swith

---
+++ switch.js
@@ -0,0 +1,23 @@
+let languange = 'english';
+let gretting = 'Terima kasih';
+
+switch (languange) {
+  case 'english':
+    gretting = 'Thank You';
+    break;
+
+  case 'french':
+    gretting = 'bonjour';
+    break;
+
+  case 'wakwak':
+    gretting = wale;
+    break;
+
+  case 'japan':
+    gretting = 'arigatou';
+    break;
+  default:
+    'Terima kasih';
+}
+console.log(gretting);
